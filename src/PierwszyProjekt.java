74bbb137b80cbaa7abfc792ebb5cfa8aa9ae2ec4
michal <michalfulara201@gmail.com>
2018-02-28 20:54:44 +0100

    commit

---
+++ src/PierwszyProjekt.java
@@ -0,0 +1,20 @@
+public class PierwszyProjekt {
+    public static void main(String[]args){
+        System.out.println("Hello");
+        int a = 4;
+        int b = 4;
+        int wynik;
+        wynik = a+b;
+
+        System.out.println(wynik);
+        if (wynik<=5 ) {
+            System.out.println("wynik mniejszy/rowny 5");
+        }
+        else {
+
+            System.out.println("wynik wiekszy niz 5");
+
+            }
+        }
+    }
+
